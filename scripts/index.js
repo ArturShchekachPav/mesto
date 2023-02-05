3a2c4367c528d2a6cc67423c3c7244e74a705472
ArturShchekachPav <arturshchekatch@yandex.ru>
2023-02-06 04:39:28 +1000

    feat: add add-pard-popup and image-popup

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,14 @@
-let formElement = document.querySelector('.popup__form');
+let formElements = document.querySelectorAll('.popup__form');
 let nameInput = document.querySelector('.popup__field_profile_name');
 let jobInput = document.querySelector('.popup__field_profile_job');
+let titleInput = document.querySelector('.popup__field_card_title');
+let linkInput = document.querySelector('.popup__field_card_link');
 let editProfileButton = document.querySelector('.profile__edit-button');
-let popup = document.querySelector('.popup');
-let closePopupButton = document.querySelector('.popup__close-button')
+let addCardButton = document.querySelector('.profile__add-button');
+let popupEditProfile = document.querySelector('.popup__profile-edit');
+let popupAddCard = document.querySelector('.popup__add-card');
+let popupImage = document.querySelector('.popup_image');
+let closePopupButtons = document.querySelectorAll('.popup__close-button');
 let profileName = document.querySelector('.profile__name');
 let profileJob = document.querySelector('.profile__job');
 let sectionElements = document.querySelector('.elements');
@@ -41,7 +46,7 @@ function renderCards(items) {
     return createCard(item)
   })
 
-  sectionElements.append(...cards)
+  sectionElements.append(...cards);
 }
 
 function createCard(item) {
@@ -51,6 +56,7 @@ function createCard(item) {
 
   card.querySelector('.element__like-button').addEventListener("click", putAlike);
   card.querySelector('.element__delete-button').addEventListener('click', deleteCard);
+  card.querySelector('.element__image').addEventListener('click', openPopup)
 
   return card;
 }
@@ -63,28 +69,52 @@ function deleteCard(evt) {
   evt.target.closest('.element').remove();
 }
 
-function openPopup() {
-  popup.classList.add('popup_opened');
+function openPopup(evt) {
+  if (evt.target === editProfileButton) {
+    popupEditProfile.classList.add('popup_opened');
 
-  nameInput.value = profileName.textContent;
-  jobInput.value = profileJob.textContent;
+    nameInput.value = profileName.textContent;
+    jobInput.value = profileJob.textContent;
+  }
+
+  if (evt.target === addCardButton) {
+    popupAddCard.classList.add('popup_opened');
+  }
+
+  if (evt.target.classList.contains('element__image')) {
+    popupImage.classList.add('popup_opened');
+
+    popupImage.querySelector('.popup__image').src = evt.target.src;
+    popupImage.querySelector('.popup__figcaption').textContent = evt.target.parentNode.querySelector('.element__title').textContent;
+  }
 }
 
-function closePopup() {
-  popup.classList.remove('popup_opened');
+function closePopup(evt) {
+  evt.target.closest('.popup').classList.remove('popup_opened');
 }
 
 function handleFormSubmit(evt) {
   evt.preventDefault();
 
-  profileName.textContent = nameInput.value;
-  profileJob.textContent = jobInput.value;
+  if(evt.target.closest('.popup').classList.contains('popup__profile-edit')) {
+    profileName.textContent = nameInput.value;
+    profileJob.textContent = jobInput.value;
+  }
 
-  closePopup();
+  if(evt.target.closest('.popup').classList.contains('popup__add-card')) {
+    sectionElements.append(createCard({name: titleInput.value, link: linkInput.value}));
+  }
+
+  closePopup(evt);
 }
 
 renderCards(initialCards);
 
 editProfileButton.addEventListener('click', openPopup);
-closePopupButton.addEventListener('click', closePopup);
-formElement.addEventListener('submit', handleFormSubmit);
+addCardButton.addEventListener('click', openPopup);
+closePopupButtons.forEach((element) => {
+  element.addEventListener('click', closePopup);
+});
+formElements.forEach((form) => {
+  form.addEventListener('submit', handleFormSubmit);
+})
